156b68ffd4d8c53554f10fc48c5369b0676b4f17
RayJay1108 <rjvargas0804@gmail.com>
2025-03-28 19:04:35 -0600

    some more css and added api for weather

---
+++ Home Page/java.js
@@ -0,0 +1,11 @@
+fetch ('https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=8c90844a9e11a877c90ae40480aebcc2&units=metric')
+
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    console.log(data.main.temp);
+    condition.textContent = "Current Condition: "+data.main.temp+"\u00B0C" +data.clouds;
+    wind.textContent = "Wind: "+ data.wind.speed + data.wind.deg + data.wind.gust;
+    
+
+});
