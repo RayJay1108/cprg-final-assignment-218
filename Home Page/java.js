af372af5cc4aa642c0a5c2dd6b9561e6e091c32b
RayJay1108 <rjvargas0804@gmail.com>
2025-04-02 14:58:24 -0600

    hamburger menu and weather icon

--- Home Page/java.js
+++ Home Page/java.js
@@ -3,8 +3,10 @@ fetch ('https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=8c90844a9
 .then(response => response.json())
 .then(data => {
     console.log(data);
-    condition.textContent = "Current Condition: "+data.main.temp+"\u00B0C" + "..." + data.clouds;
+    condition.textContent = "Current Condition: "+data.main.temp+"\u00B0C";
     wind.textContent = "Wind: "+ data.wind.speed + "..." + data.wind.deg;
+    icon.src = ("https://openweathermap.org/img/wn/") + data.weather[0].icon + "@2x.png";
+
     
 });
 
@@ -15,5 +17,15 @@ more.addEventListener('click', function(){
 less.addEventListener('click', function(){
     document.getElementById('more-testimonials').style.display = "none"; 
 });
+
+
+const toggleBTN = document.getElementById('toggle'); 
+const mainMenu = document.getElementById('navigation');
+
+toggleBTN.addEventListener('click', function() {
+    toggleBTN.classList.toggle('open');
+    mainMenu.classList.toggle('active');
+})
+
 
 
