50a6520bcc37cb3506de40245fa083410861e81f
RayJay1108 <rjvargas0804@gmail.com>
2025-04-15 12:26:26 +0800

    troubleshooting

--- Accomodation/accom.js
+++
@@ -1,7 +0,0 @@
-const toggleBTN = document.getElementById('toggle'); 
-const mainMenu = document.getElementById('navigation');
-
-toggleBTN.addEventListener('click', function() {
-    toggleBTN.classList.toggle('open');
-    mainMenu.classList.toggle('active');
-});
